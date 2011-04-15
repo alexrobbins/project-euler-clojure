6a33fa0d1312f3c2a91e9c73d312a07bc3e50228
Alan Malloy <alan@malloys.org>
2011-04-15 15:34:37 -0700

    Simplify and speed up problem 9

--- src/euler/problem009.clj
+++ src/euler/problem009.clj
@@ -9,11 +9,17 @@
 ; find (* a b c)
 ;
 
-(defn problem-9 ; brute forced :)
+(defn problem-9                         ; brute forced :)
   ([] (problem-9 1000))
   ([sum]
-   (for [a (range (dec sum)) b (range sum) c (range (inc sum))
-     :when (and (and (< a b) (< b c))
-                (= 1000 (+ a b c))
-                (= (+ (expt a 2) (expt b 2)) (expt c 2)))]
-     (* a b c))))
+     (apply *
+            (first
+             (for [a (range 1 (inc sum))
+                   :let [a2 (* a a)]
+                   b (range 1 a)
+                   :let [b2 (* b b)]
+                   c (range 1 b)
+                   :when (and (= sum (+ a b c))
+                              (= (+ (* c c) b2)
+                                 a2))]
+               [a b c])))))
