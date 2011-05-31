0f6b8e62e28a726733e683f50fbcf3998c154c4e
Alex Robbins <alexander.j.robbins@gmail.com>
2011-05-31 18:58:25 -0500

    Adding vimclojure.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-time "0.3.0-SNAPSHOT"]
-                 [clojure-csv "1.2.4"]])
+                 [clojure-csv "1.2.4"]]
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]
+                     [vimclojure/server "2.2.0"]])
